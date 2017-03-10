45177bf2f384810d64f8582508cc4d57be6da8cd
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-09 20:30:03 -0800

    changed script tag to it links to html

---
+++ app.js
@@ -0,0 +1,23 @@
+
+'use strict'
+// Object stored in an array.
+var cocktailNames = ['classic-martini', 'manhattan'];
+
+var cocktailRecipeBox = [];
+
+// Constructor Function
+function Cocktail(name, imagePath, ingredients, recipe){
+  this.name = name;
+  // this.imagePath = 'assets/' + name + '.jpg';
+  // this.ingredient1 = ingredient1;
+  // this.ingredient2 = ingredient2;
+  // this.ingredient3 = ingredient3;
+  // this.recipe = recipe;
+  cocktailRecipeBox.push(this);
+}
+(function() {
+
+  for (var i in cocktailNames){
+    var newCocktail = new Cocktail(cocktailNames[i])
+  };
+})()
