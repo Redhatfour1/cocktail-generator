a9d1bc040c9f477f80bb1e8e684292e88d5328e8
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-10 22:45:55 -0800

    i got the ingridients stored in selectedingredientsArray

--- app.js
+++ app.js
@@ -29,4 +29,31 @@ cocktailRecipeBox[1].ingredient2 = 'Red Vermouth';
 cocktailRecipeBox[1].ingredient3 = 'Bitters';
 cocktailRecipeBox[1].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/';
 
-form.addEventListener('submit')
+
+
+function handleFormSubmit(event) {
+  // event.preventDefault();
+  var selectedIngredientsArray = [];
+
+  var elem1 = document.getElementById("ing1");
+  var selectedNode1 = elem1.options[elem1.selectedIndex].value;
+  console.log(selectedNode1);
+  selectedIngredientsArray.push(selectedNode1);
+
+  var elem2 = document.getElementById("ing2");
+  var selectedNode2 = elem2.options[elem2.selectedIndex].value;
+  console.log(selectedNode2);
+  selectedIngredientsArray.push(selectedNode2);
+
+  var elem3 = document.getElementById("ing3");
+  var selectedNode3 = elem3.options[elem3.selectedIndex].value;
+  console.log(selectedNode3);
+  selectedIngredientsArray.push(selectedNode3);
+
+  console.log();
+  return selectedIngredientsArray;
+
+}
+
+document.getElementById('submit').addEventListener('submit', handleFormSubmit);
+console.log(event);
