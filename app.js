91c76582b61e4be8e2fb36b338fcf1825f68116b
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-10 23:42:45 -0800

    Added a logical operator as a place holder, need something differnt. Good night

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ function Cocktail(name, imagePath, ingredient1, ingredient2, ingredient3){
 // Classic Martini
 cocktailRecipeBox[0].ingredient1 = 'Vermouth';
 cocktailRecipeBox[0].ingredient2 = 'Gin';
+cocktailRecipeBox[0].ingredient2 = 'None';
 cocktailRecipeBox[0].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/whiskey-sour/';
 // Manhattan
 
@@ -29,31 +30,37 @@ cocktailRecipeBox[1].ingredient2 = 'Red Vermouth';
 cocktailRecipeBox[1].ingredient3 = 'Bitters';
 cocktailRecipeBox[1].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/';
 
-
+var selectedIngredientsArray = [];
 
 function handleFormSubmit(event) {
   // event.preventDefault();
-  var selectedIngredientsArray = [];
+  selectedIngredientsArray = [];
 
-  var elem1 = document.getElementById("ing1");
-  var selectedNode1 = elem1.options[elem1.selectedIndex].value;
-  console.log(selectedNode1);
+  var getIngredientOne = document.getElementById("ing1");
+  var selectedNode1 = getIngredientOne.options[getIngredientOne.selectedIndex].value;
   selectedIngredientsArray.push(selectedNode1);
 
   var elem2 = document.getElementById("ing2");
   var selectedNode2 = elem2.options[elem2.selectedIndex].value;
-  console.log(selectedNode2);
   selectedIngredientsArray.push(selectedNode2);
 
   var elem3 = document.getElementById("ing3");
   var selectedNode3 = elem3.options[elem3.selectedIndex].value;
-  console.log(selectedNode3);
   selectedIngredientsArray.push(selectedNode3);
 
-  console.log();
+  console.log(selectedIngredientsArray);
   return selectedIngredientsArray;
 
 }
 
-document.getElementById('submit').addEventListener('submit', handleFormSubmit);
+console.log(handleFormSubmit());
+
+for (var i in selectedIngredientsArray) {
+  if (selectedIngredientsArray[0].Cocktail.indexOf(cocktailRecipeBox) === -1 && selectedIngredientsArray[1].Cocktail.indexOf(cocktailRecipeBox) === -1 && selectedIngredientsArray[2].Cocktail.indexOf(cocktailRecipeBox) === -1){
+    console.log('foo');
+  }
+};
+
+
+document.getElementById('submit').addEventListener('click', handleFormSubmit);
 console.log(event);
