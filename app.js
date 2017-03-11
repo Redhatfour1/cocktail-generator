e2a1652250fc3caa2d124ae619464dce93c4fe91
Louis W. Haywood <louis@Louiss-MacBook-Pro.local>
2017-03-11 13:33:50 -0800

    added 10 more drinks to js which equals a total of 20

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ function Cocktail(name, imagePath, ingredient1, ingredient2, ingredient3){
 
 // 1.Classic Martini
 cocktailRecipeBox[0].ingredient1 = 'Gin';
-cocktailRecipeBox[0].ingredient2 = 'Vermouth';
+cocktailRecipeBox[0].ingredient2 = 'White Vermouth';
+cocktailRecipeBox[0].ingredient3 = 'None';
 cocktailRecipeBox[0].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/whiskey-sour/';
 
 // 2.Manhattan
@@ -30,7 +31,7 @@ cocktailRecipeBox[1].ingredient3 = 'Bitters';
 cocktailRecipeBox[1].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/';
 
 // 3.Margarita
-cocktailRecipeBox[2].ingredient1 = 'Tequila';
+cocktailRecipeBox[2].ingredient1 = 'White Tequila';
 cocktailRecipeBox[2].ingredient2 = 'Cointreau';
 cocktailRecipeBox[2].ingredient3 = 'Lime Juice';
 cocktailRecipeBox[2].link = 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html';
@@ -73,6 +74,66 @@ cocktailRecipeBox[8].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails
 
 // 10.Brandy Alexander
 cocktailRecipeBox[9].ingredient1 = 'Brandy';
-cocktailRecipeBox[9].ingredient2 = 'Dark Cre`me De Liquer';
+cocktailRecipeBox[9].ingredient2 = 'Dark Créme De Liquer';
 cocktailRecipeBox[9].ingredient3 = 'Single Cream';
 cocktailRecipeBox[9].link = 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/brandy-alexander/';
+
+// 11.Adult Hot Chocolate
+cocktailRecipeBox[10].ingredient1 = 'Peppermint Snapps';
+cocktailRecipeBox[10].ingredient2 = 'Hot Chocolate';
+cocktailRecipeBox[10].ingredient3 = 'Whip Cream';
+cocktailRecipeBox[10].link = 'http://www.drinksmixer.com/drinkn1m2540.html';
+
+// 12.White Sangria
+cocktailRecipeBox[11].ingredient1 = 'White Wine';
+cocktailRecipeBox[11].ingredient2 = 'Calvados';
+cocktailRecipeBox[11].ingredient3 = 'Fruit';
+cocktailRecipeBox[11].link = 'http://www.foodnetwork.com/recipes/rachael-ray/white-sangria-recipe';
+
+// 13.Raspberry Prosecco Bellini
+cocktailRecipeBox[12].ingredient1 = 'Prosecco';
+cocktailRecipeBox[12].ingredient2 = 'Raspberry liqueur';
+cocktailRecipeBox[12].ingredient3 = 'Basil';
+cocktailRecipeBox[12].link = 'http://www.liquidgolddrinks.com/cocktails/prosecco-cocktails/raspberry-prosecco-bellini/';
+
+// 14.Almond Joy
+cocktailRecipeBox[13].ingredient1 = 'Coconut Rum';
+cocktailRecipeBox[13].ingredient2 = 'Amaretto';
+cocktailRecipeBox[13].ingredient3 = 'Crème de cacao';
+cocktailRecipeBox[13].link = 'http://allrecipes.com/recipe/234754/almond-joy-cocktail/';
+
+// 15.Sparkling Suze
+cocktailRecipeBox[14].ingredient1 = 'Suze';
+cocktailRecipeBox[14].ingredient2 = 'St Germain\'s';
+cocktailRecipeBox[14].ingredient3 = 'Cava';
+cocktailRecipeBox[14].link = 'http://www.seriouseats.com/recipes/2014/08/suze-cava-st-germain-cocktail-aperitif-drink.html';
+
+// 16.Man About Town 
+cocktailRecipeBox[15].ingredient1 = 'Rye Whiskey';
+cocktailRecipeBox[15].ingredient2 = 'Cynar';
+cocktailRecipeBox[15].ingredient3 = 'Sweet Vermouth';
+cocktailRecipeBox[15].link = 'http://www.seriouseats.com/recipes/2012/10/gramercy-tavern-man-about-town-cocktail-rye-boulevardier-cynar-recipe.html';
+
+// 17.Bahia Breeze
+cocktailRecipeBox[16].ingredient1 = 'Gold Tequila';
+cocktailRecipeBox[16].ingredient2 = 'Pineapple Juice';
+cocktailRecipeBox[16].ingredient3 = 'Cranberry';
+cocktailRecipeBox[16].link = 'http://www.drinksmixer.com/drink7265.html';
+
+// 18.The Spitfire
+cocktailRecipeBox[17].ingredient1 = 'Mezcal';
+cocktailRecipeBox[17].ingredient2 = 'Green Chartreuse';
+cocktailRecipeBox[17].ingredient3 = 'Tabasco Sauce';
+cocktailRecipeBox[17].link = 'http://www.barnonedrinks.com/drinks/t/the-spitfire-7751.html';
+
+// 19.Angry Balls
+cocktailRecipeBox[18].ingredient1 = 'Fireball Whiskey';
+cocktailRecipeBox[18].ingredient2 = 'Hard Cider';
+cocktailRecipeBox[18].ingredient3 = 'None';
+cocktailRecipeBox[18].link = 'http://fireballwhisky.com/recipes/angry-balls/';
+
+// 20.Chrysanthemum Cocktail
+cocktailRecipeBox[19].ingredient1 = 'Bénédictine';
+cocktailRecipeBox[19].ingredient2 = 'Absinthe';
+cocktailRecipeBox[19].ingredient3 = 'Dry Vermouth';
+cocktailRecipeBox[19].link = 'http://imbibemagazine.com/chrysanthemum-cocktail-recipe/';
