43618ee81896e6f19e4a0db07b7d75a96e15667c
Louis W. Haywood <louis@Louiss-MacBook-Pro.local>
2017-03-15 02:19:03 -0700

    coded 15 more drinks into all 5 arrays and fixed the video to have full view and not just the top

--- app.js
+++ app.js
@@ -2,17 +2,33 @@
 'use strict'
 
 //Cocktail Object stored in an array.
-var cocktailNames = ['Classic Martini', 'Manhattan', 'Margarita', 'Japanese Cocktail', 'Great Gatsby', 'Dark & Stormy', 'Campari Spritz', 'Adriatique', 'Mint Julep', 'Brandy Alexander', 'Adult Hot Chocolate', 'White Sangria', 'Cuba Libre', 'Raspberry Prosecco Bellini', 'Rum & Coke'];
+var cocktailNames = ['Classic Martini', 'Manhattan', 'Margarita', 'Japanese Cocktail', 'Great Gatsby', 'Dark & Stormy', 'Campari Spritz', 'Adriatique', 'Mint Julep', 'Brandy Alexander', 'Adult Hot Chocolate', 'White Sangria', 'Cuba Libre', 'Raspberry Prosecco Bellini', 'Rum & Coke', 'Almond Joy Cocktail', 'Sparkling Suze', 'Man About Town', 'Bahia Breeze', 'The Spitfire', 'Angry Balls', 'Chrysanthemum Cocktail', 'The Obituary', 'The Old Fashioned', 'Frisco Sour Recipe', 'Boulevardier', 'Gold Rush', 'Grapefruit & Ginger Sparkler', 'Blushing Betty', 'Daiquiri'];
 
 //Ingredients have to be in same order as drink
-var ingredient1Array = ['Gin', 'Whiskey', 'White Tequila', 'Cognac', 'Vodka', 'Rum', 'Sparkling Wine', 'Amaro Montenegro', 'Bourbon', 'Brandy', 'Peppermint Snapps', 'White Wine', 'Rum', 'Prosecco','Rum'];
+var ingredient1Array = ['Gin', 'Whiskey', 'White Tequila', 'Cognac', 'Vodka', 'Rum', 'Sparkling Wine', 'Amaro Montenegro', 'Bourbon', 'Brandy', 'Peppermint Snapps', 'White Wine', 'Rum', 'Prosecco','Rum', 'Coconut Rum', 'Suze', 'Rye Whiskey', 'Gold Tequila', 'Mezcal', 'Fireball Whiskey', 'Bénédictine', 'Gin', 'Rye Whiskey', 'Rye Whiskey', 'Bourbon', 'Bourbon', 'Ginger Liqueur', 'Bourbon', 'Light Rum'];
 
-var ingredient2Array = ['White Vermouth', 'Red Vermouth', 'Cointreau', 'Orgeat', 'Lillet Blanc', 'Ginger Beer', 'Campari', 'Aperol', 'Mint Leaves', 'Dark Créme De Liquer', 'Hot Chocolate', 'Calvados', 'Coke', 'Raspberry liqueur', 'Coke'];
+var ingredient2Array = ['White Vermouth', 'Red Vermouth', 'Cointreau', 'Orgeat', 'Lillet Blanc', 'Ginger Beer', 'Campari', 'Aperol', 'Mint Leaves', 'Dark Créme De Liquer', 'Hot Chocolate', 'Calvados', 'Coke', 'Raspberry liqueur', 'Coke', 'Amaretto', 'St Germain\'s', 'Cynar', 'Pineapple Juice', 'Green Chartreuse', 'Hard Cider', 'Absinthe', 'Dry Vermouth', 'Angostura', 'Benedictine', 'Campari', 'Lemon Juice', 'Prosecco', 'Simple Syrup', 'Lime Juice'];
 
-var ingredient3Array = ['None', 'Bitters', 'Lime Juice', 'Angostura Bitters', 'Grapefruit Juice', 'Lime', 'Club Soda', 'Orange Juice', 'Sugar', 'Single Cream', 'Whip Cream', 'Fruit', 'Lime', 'Basil', 'None'];
+var ingredient3Array = ['None', 'Bitters', 'Lime Juice', 'Angostura Bitters', 'Grapefruit Juice', 'Lime', 'Club Soda', 'Orange Juice', 'Sugar', 'Single Cream', 'Whip Cream', 'Fruit', 'Lime', 'Basil', 'None', 'Crème de Cacao', 'Cava', 'Sweet Vermouth', 'Cranberry', 'Tabasco Sauce', 'None', 'Dry Vermouth', 'Absinthe', 'Sugar', 'Lemon Juice', 'Sweet Vermouth', 'Honey', 'Grapefruit Juice', 'Pink Grapefruit', 'Sugar'];
 
-var linkArray = ['http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/martini/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/', 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html', 'http://www.seriouseats.com/recipes/2011/11/japanese-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/frank-stitts-great-gatsby-vodka-grapefruit-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/dark-and-stormy-cocktail-the-spotted-pig-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/campari-spritz-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/jackson-cannons-adriatique-aperitif-aperol-orange-juice-amaro-cocktail-recipe.html','http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/mint-julep/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/brandy-alexander/', 'http://www.drinksmixer.com/drinkn1m2540.html'
-,'http://www.foodnetwork.com/recipes/rachael-ray/white-sangria-recipe', 'http://www.seriouseats.com/recipes/2012/01/cuba-libre-rum-coke-cocktail-recipe.html',  'http://www.liquidgolddrinks.com/cocktails/prosecco-cocktails/raspberry-prosecco-bellini/']
+var linkArray = ['http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/martini/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/', 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html', 'http://www.seriouseats.com/recipes/2011/11/japanese-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/frank-stitts-great-gatsby-vodka-grapefruit-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/dark-and-stormy-cocktail-the-spotted-pig-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/campari-spritz-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/jackson-cannons-adriatique-aperitif-aperol-orange-juice-amaro-cocktail-recipe.html','http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/mint-julep/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/brandy-alexander/', 'http://www.drinksmixer.com/drinkn1m2540.html',
+,'http://www.foodnetwork.com/recipes/rachael-ray/white-sangria-recipe', 'http://www.seriouseats.com/recipes/2012/01/cuba-libre-rum-coke-cocktail-recipe.html',  'http://www.liquidgolddrinks.com/cocktails/prosecco-cocktails/raspberry-prosecco-bellini/',
+'https://www.thespruce.com/rum-and-coke-recipe-760560',
+'http://allrecipes.com/recipe/234754/almond-joy-cocktail/',
+'http://www.seriouseats.com/recipes/2014/08/suze-cava-st-germain-cocktail-aperitif-drink.html',
+'http://www.seriouseats.com/recipes/2012/10/gramercy-tavern-man-about-town-cocktail-rye-boulevardier-cynar-recipe.html',
+'http://www.drinksmixer.com/drink7265.html',
+'http://www.barnonedrinks.com/drinks/t/the-spitfire-7751.html',
+'http://fireballwhisky.com/recipes/angry-balls/',
+'http://imbibemagazine.com/chrysanthemum-cocktail-recipe/',
+'http://www.seriouseats.com/recipes/2011/02/the-obituary-cocktail-gin-absinthe-vermouth.html',
+'http://www.seriouseats.com/recipes/2008/06/traditional-old-fashioned-recipe.html',
+'http://www.seriouseats.com/recipes/2013/02/frisco-sour-simple-rye-cocktail-lemon-benedictine-recipe.html',
+'http://www.seriouseats.com/recipes/2008/09/boulevardier-recipe.html',
+'http://www.seriouseats.com/recipes/2015/03/gold-rush-bourbon-honey-cocktail-drink-recipe.html',
+'http://www.seriouseats.com/recipes/2012/02/drinking-in-season-grapefruit-ginger-sparkling-wine-cocktail-recipe.html',
+'http://www.seriouseats.com/recipes/2014/03/simple-cocktail-blushing-betty-bourbon-grapefruit-easy-drink-recipe.html',
+'http://www.seriouseats.com/recipes/2008/05/daiquiri-recipe.html']
 
 var cocktailRecipeBox = [];
 console.log(cocktailRecipeBox);
@@ -35,55 +51,6 @@ function Cocktail(name, imagePath, ingredient1, ingredient2, ingredient3, link){
     ingredient3Array[i], linkArray[i])
   };
 })()
-
-
-
-
-//
-// // 14.
-// cocktailRecipeBox[13].ingredient1 = 'Coconut Rum';
-// cocktailRecipeBox[13].ingredient2 = 'Amaretto';
-// cocktailRecipeBox[13].ingredient3 = 'Crème de cacao';
-// cocktailRecipeBox[13].link = 'http://allrecipes.com/recipe/234754/almond-joy-cocktail/';
-//
-// // 15.Sparkling Suze
-// cocktailRecipeBox[14].ingredient1 = 'Suze';
-// cocktailRecipeBox[14].ingredient2 = 'St Germain\'s';
-// cocktailRecipeBox[14].ingredient3 = 'Cava';
-// cocktailRecipeBox[14].link = 'http://www.seriouseats.com/recipes/2014/08/suze-cava-st-germain-cocktail-aperitif-drink.html';
-//
-// // 16.Man About Town 
-// cocktailRecipeBox[15].ingredient1 = 'Rye Whiskey';
-// cocktailRecipeBox[15].ingredient2 = 'Cynar';
-// cocktailRecipeBox[15].ingredient3 = 'Sweet Vermouth';
-// cocktailRecipeBox[15].link = 'http://www.seriouseats.com/recipes/2012/10/gramercy-tavern-man-about-town-cocktail-rye-boulevardier-cynar-recipe.html';
-//
-// // 17.Bahia Breeze
-// cocktailRecipeBox[16].ingredient1 = 'Gold Tequila';
-// cocktailRecipeBox[16].ingredient2 = 'Pineapple Juice';
-// cocktailRecipeBox[16].ingredient3 = 'Cranberry';
-// cocktailRecipeBox[16].link = 'http://www.drinksmixer.com/drink7265.html';
-//
-// // 18.The Spitfire
-// cocktailRecipeBox[17].ingredient1 = 'Mezcal';
-// cocktailRecipeBox[17].ingredient2 = 'Green Chartreuse';
-// cocktailRecipeBox[17].ingredient3 = 'Tabasco Sauce';
-// cocktailRecipeBox[17].link = 'http://www.barnonedrinks.com/drinks/t/the-spitfire-7751.html';
-//
-// // 19.Angry Balls
-// cocktailRecipeBox[18].ingredient1 = 'Fireball Whiskey';
-// cocktailRecipeBox[18].ingredient2 = 'Hard Cider';
-// cocktailRecipeBox[18].ingredient3 = 'None';
-// cocktailRecipeBox[18].link = 'http://fireballwhisky.com/recipes/angry-balls/';
-//
-// // 20.Chrysanthemum Cocktail
-// cocktailRecipeBox[19].ingredient1 = 'Bénédictine';
-// cocktailRecipeBox[19].ingredient2 = 'Absinthe';
-// cocktailRecipeBox[19].ingredient3 = 'Dry Vermouth';
-// cocktailRecipeBox[19].link = 'http://imbibemagazine.com/chrysanthemum-cocktail-recipe/';
-
-
-
 
 
 var userSelectedIngredientsArray = [];
