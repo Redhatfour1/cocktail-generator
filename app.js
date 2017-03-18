1134ca1ee7ae76ec8ca6e043e63be7cfab555b06
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-18 02:02:40 -0700

    Added lots on cocktail pngs

--- app.js
+++ app.js
@@ -2,17 +2,42 @@
 'use strict'
 
 //Cocktail Object stored in an array.
-var cocktailNames = ['Classic Martini', 'Manhattan', 'Margarita', 'Japanese Cocktail', 'Great Gatsby', 'Dark & Stormy', 'Campari Spritz', 'Adriatique', 'Mint Julep', 'Brandy Alexander', 'Adult Hot Chocolate', 'White Sangria', 'Cuba Libre', 'Raspberry Prosecco Bellini', 'Rum & Coke'];
+var cocktailNames = ['Classic Martini', 'Manhattan', 'Margarita', 'Japanese Cocktail', 'Great Gatsby', 'Dark & Stormy', 'Campari Spritz', 'Adriatique', 'Mint Julep', 'Brandy Alexander', 'Adult Hot Chocolate', 'White Sangria', 'Cuba Libre', 'Raspberry Prosecco Bellini', 'Rum & Coke', 'Almond Joy Cocktail', 'Sparkling Suze', 'Man About Town', 'Bahia Breeze', 'The Spitfire', 'Angry Balls', 'Chrysanthemum Cocktail', 'The Obituary', 'The Old Fashioned', 'Frisco Sour Recipe', 'Boulevardier', 'Gold Rush', 'Grapefruit & Ginger Sparkler', 'Blushing Betty', 'Daiquiri', 'Butterbeer', 'Pomegranate Champagne Cocktail', 'Blood Orange Rickey', 'Salty Dog', 'Coffee Martini'];
 
 //Ingredients have to be in same order as drink
-var ingredient1Array = ['Gin', 'Whiskey', 'White Tequila', 'Cognac', 'Vodka', 'Rum', 'Sparkling Wine', 'Amaro Montenegro', 'Bourbon', 'Brandy', 'Peppermint Snapps', 'White Wine', 'Rum', 'Prosecco','Rum'];
 
-var ingredient2Array = ['White Vermouth', 'Red Vermouth', 'Cointreau', 'Orgeat', 'Lillet Blanc', 'Ginger Beer', 'Campari', 'Aperol', 'Mint Leaves', 'Dark Créme De Liquer', 'Hot Chocolate', 'Calvados', 'Coke', 'Raspberry liqueur', 'Coke'];
+var ingredient1Array = ['Gin', 'Whiskey', 'White Tequila', 'Cognac', 'Vodka', 'Rum', 'Sparkling Wine', 'Amaro Montenegro', 'Bourbon', 'Brandy', 'Peppermint Snapps', 'White Wine', 'Rum', 'Prosecco','Rum', 'Coconut Rum', 'Suze', 'Rye Whiskey', 'Gold Tequila', 'Mezcal', 'Fireball Whiskey', 'Bénédictine', 'Gin', 'Rye Whiskey', 'Rye Whiskey', 'Bourbon', 'Bourbon', 'Ginger Liqueur', 'Bourbon', 'Light Rum', 'Pumpkin Ale', 'Pomegranate Liqueur', 'Gin', 'Vodka', 'Gin'];
 
-var ingredient3Array = ['None', 'Bitters', 'Lime Juice', 'Angostura Bitters', 'Grapefruit Juice', 'Lime', 'Club Soda', 'Orange Juice', 'Sugar', 'Single Cream', 'Whip Cream', 'Fruit', 'Lime', 'Basil', 'None'];
+var ingredient2Array = ['White Vermouth', 'Red Vermouth', 'Cointreau', 'Orgeat', 'Lillet Blanc', 'Ginger Beer', 'Campari', 'Aperol', 'Mint Leaves', 'Dark Créme De Liquer', 'Hot Chocolate', 'Calvados', 'Coke', 'Raspberry liqueur', 'Coke', 'Amaretto', 'St Germain\'s', 'Cynar', 'Pineapple Juice', 'Green Chartreuse', 'Hard Cider', 'Absinthe', 'Dry Vermouth', 'Angostura', 'Benedictine', 'Campari', 'Lemon Juice', 'Prosecco', 'Simple Syrup', 'Lime Juice', 'Butterscotch Sauce', 'Lemon', 'Sparkling Water', 'Grapefruit Juice', 'Amaretto Liquor'];
 
-var linkArray = ['http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/martini/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/', 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html', 'http://www.seriouseats.com/recipes/2011/11/japanese-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/frank-stitts-great-gatsby-vodka-grapefruit-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/dark-and-stormy-cocktail-the-spotted-pig-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/campari-spritz-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/jackson-cannons-adriatique-aperitif-aperol-orange-juice-amaro-cocktail-recipe.html','http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/mint-julep/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/brandy-alexander/', 'http://www.drinksmixer.com/drinkn1m2540.html'
-,'http://www.foodnetwork.com/recipes/rachael-ray/white-sangria-recipe', 'http://www.seriouseats.com/recipes/2012/01/cuba-libre-rum-coke-cocktail-recipe.html',  'http://www.liquidgolddrinks.com/cocktails/prosecco-cocktails/raspberry-prosecco-bellini/']
+
+var ingredient3Array = ['None', 'Bitters', 'Lime Juice', 'Angostura Bitters', 'Grapefruit Juice', 'Lime', 'Club Soda', 'Orange Juice', 'Sugar', 'Single Cream', 'Whip Cream', 'Fruit', 'Lime', 'Basil', 'None', 'Crème de Cacao', 'Cava', 'Sweet Vermouth', 'Cranberry', 'Tabasco Sauce', 'None', 'Dry Vermouth', 'Absinthe', 'Sugar', 'Lemon Juice', 'Sweet Vermouth', 'Honey', 'Grapefruit Juice', 'Pink Grapefruit', 'Sugar', 'Vanilla Cream Soda', 'None', 'Blood Oranges', 'Salt', 'Coffee'];
+
+var linkArray = ['http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/martini/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/manhattan/', 'http://www.seriouseats.com/recipes/2015/04/classic-margarita-recipe-tequila-cocktail.html', 'http://www.seriouseats.com/recipes/2011/11/japanese-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/frank-stitts-great-gatsby-vodka-grapefruit-cocktail-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/dark-and-stormy-cocktail-the-spotted-pig-recipe.html', 'http://www.seriouseats.com/recipes/2011/06/campari-spritz-recipe.html', 'http://www.seriouseats.com/recipes/2012/01/jackson-cannons-adriatique-aperitif-aperol-orange-juice-amaro-cocktail-recipe.html',
+'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/mint-julep/', 'http://www.telegraph.co.uk/food-and-drink/cocktails/10-classic-mad-men-era-cocktails/brandy-alexander/',
+'http://www.drinksmixer.com/drinkn1m2540.html',
+'http://www.foodnetwork.com/recipes/rachael-ray/white-sangria-recipe', 'http://www.seriouseats.com/recipes/2012/01/cuba-libre-rum-coke-cocktail-recipe.html',  'http://www.liquidgolddrinks.com/cocktails/prosecco-cocktails/raspberry-prosecco-bellini/',
+'https://www.thespruce.com/rum-and-coke-recipe-760560',
+'http://allrecipes.com/recipe/234754/almond-joy-cocktail/',
+'http://www.seriouseats.com/recipes/2014/08/suze-cava-st-germain-cocktail-aperitif-drink.html',
+'http://www.seriouseats.com/recipes/2012/10/gramercy-tavern-man-about-town-cocktail-rye-boulevardier-cynar-recipe.html',
+'http://www.drinksmixer.com/drink7265.html',
+'http://www.barnonedrinks.com/drinks/t/the-spitfire-7751.html',
+'http://fireballwhisky.com/recipes/angry-balls/',
+'http://imbibemagazine.com/chrysanthemum-cocktail-recipe/',
+'http://www.seriouseats.com/recipes/2011/02/the-obituary-cocktail-gin-absinthe-vermouth.html',
+'http://www.seriouseats.com/recipes/2008/06/traditional-old-fashioned-recipe.html',
+'http://www.seriouseats.com/recipes/2013/02/frisco-sour-simple-rye-cocktail-lemon-benedictine-recipe.html',
+'http://www.seriouseats.com/recipes/2008/09/boulevardier-recipe.html',
+'http://www.seriouseats.com/recipes/2015/03/gold-rush-bourbon-honey-cocktail-drink-recipe.html',
+'http://www.seriouseats.com/recipes/2012/02/drinking-in-season-grapefruit-ginger-sparkling-wine-cocktail-recipe.html',
+'http://www.seriouseats.com/recipes/2014/03/simple-cocktail-blushing-betty-bourbon-grapefruit-easy-drink-recipe.html',
+'http://www.seriouseats.com/recipes/2008/05/daiquiri-recipe.html',
+'https://www.popsugar.com/food/Hot-Cold-Butterbeer-Recipe-380247',
+'https://www.popsugar.com/food/Pomegranate-Champagne-Cocktail-26435241',
+'http://youngaustinian.com/2014/02/13/modern-austen-blood-orange-rickey/',
+'https://www.popsugar.com/food/Salty-Dog-Cocktail-Recipe-2674478',
+'https://www.popsugar.com/food/Coffee-Martini-37944195']
 
 var cocktailRecipeBox = [];
 console.log(cocktailRecipeBox);
